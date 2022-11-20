cb715f3f4e77633dd74555ba2fcc89a662e0000e
brij123k <brijk247@gmail.com>
2022-11-20 17:13:19 +0530

    Init

--- src/component/Card/card.js
+++ src/component/Card/card.js
@@ -1,11 +1,69 @@
 import React from 'react'
 import '../Card/card.css'
-import Imge from'../Card/image .png'
+import Imge from '../Card/image .png'
 const card = () => {
   return (
     <div className='container_card'>
-        <div> <img src={Imge} alt=''/></div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
         <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol'>Petrol</div>
+          <div className='Petrol'>Manual</div>
+          <div className='Petrol'>34,000km</div>
+        </div>
+      </div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
+        <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol'>Petrol</div>
+          <div className='Petrol'>Manual</div>
+          <div className='Petrol'>34,000km</div>
+        </div>
+      </div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
+        <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol'>Petrol</div>
+          <div className='Petrol'>Manual</div>
+          <div className='Petrol'>34,000km</div>
+        </div>
+      </div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
+        <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol'>Petrol</div>
+          <div className='Petrol'>Manual</div>
+          <div className='Petrol'>34,000km</div>
+        </div>
+      </div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
+        <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol'>Petrol</div>
+          <div className='Petrol'>Manual</div>
+          <div className='Petrol'>34,000km</div>
+        </div>
+      </div>
     </div>
   )
 }
