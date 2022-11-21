5d4e5bbc6f6ccbe67e2824879bb37ff2df993f70
brij123k <brijk247@gmail.com>
2022-11-21 19:29:06 +0530

    init

--- src/component/Card/card.js
+++ src/component/Card/card.js
@@ -11,9 +11,9 @@ const card = () => {
         <div className='price'>₹3,50,000</div>
         </div>
         <div className='item_card1'>
-          <div className='Petrol'>Petrol</div>
-          <div className='Petrol'>Manual</div>
-          <div className='Petrol'>34,000km</div>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
         </div>
       </div>
       <div className='Card'>
@@ -23,9 +23,9 @@ const card = () => {
         <div className='price'>₹3,50,000</div>
         </div>
         <div className='item_card1'>
-          <div className='Petrol'>Petrol</div>
-          <div className='Petrol'>Manual</div>
-          <div className='Petrol'>34,000km</div>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
         </div>
       </div>
       <div className='Card'>
@@ -35,9 +35,9 @@ const card = () => {
         <div className='price'>₹3,50,000</div>
         </div>
         <div className='item_card1'>
-          <div className='Petrol'>Petrol</div>
-          <div className='Petrol'>Manual</div>
-          <div className='Petrol'>34,000km</div>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
         </div>
       </div>
       <div className='Card'>
@@ -47,9 +47,9 @@ const card = () => {
         <div className='price'>₹3,50,000</div>
         </div>
         <div className='item_card1'>
-          <div className='Petrol'>Petrol</div>
-          <div className='Petrol'>Manual</div>
-          <div className='Petrol'>34,000km</div>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
         </div>
       </div>
       <div className='Card'>
@@ -59,9 +59,21 @@ const card = () => {
         <div className='price'>₹3,50,000</div>
         </div>
         <div className='item_card1'>
-          <div className='Petrol'>Petrol</div>
-          <div className='Petrol'>Manual</div>
-          <div className='Petrol'>34,000km</div>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
+        </div>
+      </div>
+      <div className='Card'>
+        <div className='img_card'> <img src={Imge} alt='' /></div>
+        <div className='item_card'>
+        <div className='Vname'>Honda City</div>
+        <div className='price'>₹3,50,000</div>
+        </div>
+        <div className='item_card1'>
+          <div className='Petrol1'>Petrol</div>
+          <div className='Petrol2'>Manual</div>
+          <div className='Petrol3'>34,000km</div>
         </div>
       </div>
     </div>
